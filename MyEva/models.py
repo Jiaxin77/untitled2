a91dec05c245f41032d2c7740a58cb24e5878313
Karroy77 <liujiaxin_bj@126.com>
2019-03-13 21:40:10 +0800

    数据库部分

--- MyEva/models.py
+++ MyEva/models.py
@@ -1,8 +1,42 @@
 from django.db import models
 
 # Create your models here.
+#not null是默认的
+class UserList(models.Model):
+    UserId = models.CharField(max_length=50,primary_key=True)
+    UserName = models.CharField(max_length=50)
+    Password = models.CharField(max_length=50)
+    Status = models.IntegerField(default=0)
 
-class User(models.Model):
-    username = models.CharField(max_length=200)
-    password = models.CharField(max_length=200)
-    seat = models.IntegerField(default=0)
+class SurveyList(models.Model):
+    SurveyId = models.CharField(max_length=50,primary_key=True)
+    AssessId = models.CharField(max_length=50)
+    SurveyName = models.CharField(max_length=50)
+    SurveyPro = models.IntegerField(default=0)
+    SurveyQueNum = models.IntegerField(default=0,null=True)
+
+class QuestionList(models.Model):
+    QuestionId = models.CharField(max_length=50,primary_key=True)
+    QueDescription = models.CharField(max_length=500,null=True)
+    QuestionType = models.IntegerField(default=0)
+    isMust = models.BooleanField(blank=True)
+    SurveyId = models.CharField(max_length=50)
+
+class ChoiceList(models.Model):
+    QuestionId = models.ForeignKey(QuestionList)
+    SCQorMCQ = models.IntegerField(default=0)
+    ChoiceA = models.CharField(max_length=50)
+    ChoiceB = models.CharField(max_length=50)
+    ChoiceC = models.CharField(max_length=50)
+    ChoiceD = models.CharField(max_length=50)
+
+class ScaleList(models.Model):
+    QuestionId = models.ForeignKey(QuestionList)
+    BeginIndex = models.CharField(max_length=50)
+    EndIndex = models.CharField(max_length=50)
+    DegreeNum = models.IntegerField(default=0)
+
+class PaperList(models.Model):
+
+
+
